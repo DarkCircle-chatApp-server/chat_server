d8f22108bc4cccb34f10b230a3d97333bccd14a7
pgh157 <kh813love@naver.com>
2025-04-01 16:45:26 +0900

    [fix]불필요제거

--- chat-admin/main.cpp
+++ chat-admin/main.cpp
@@ -25,7 +25,7 @@ int main() {
         });
 
     Select_delete user_select(db.getConnection());                                               // 개인 조회 GET 요청 처리
-    // POST 요청을 위한 엔드포인트 설정
+                                                                                                 // 입력받을 body가 있어야되서 post 사용함.
     svr.Post("/chat/admin/user_select", [&](const httplib::Request& req, httplib::Response& res) {
         user_select.handle_user_select(req, res);
         });
