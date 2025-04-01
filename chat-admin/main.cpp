d83043f34ecc9ba6a5258bffe2546e175721e979
phm0423 <qkrgusalsok@gmail.com>
2025-04-01 15:10:26 +0900

    [feature] change_PW
    비밀번호 변경 기능 추가

--- chat-admin/main.cpp
+++ chat-admin/main.cpp
@@ -3,6 +3,7 @@
 #include"DB_admin.hpp"
 #include "chat_ban.hpp"
 #include "admin_select_delete.hpp"
+#include "change_pw.hpp"
 
 // 채팅 관리자 기능 함수
 //void handle_chat_admin(const httplib::Request& req, httplib::Response& res) {
@@ -43,6 +44,11 @@ int main() {
     User_ban user_unban(db.getConnection());                                                // 밴해제 처리
     svr.Put("/chat/admin/unban", [&](const httplib::Request& req, httplib::Response& res) {
         user_unban.handle_user_unban(req, res);
+        });
+
+    Change_PW change_pw(db.getConnection());
+    svr.Put("/chat/admin/change_pw", [&](const httplib::Request& req, httplib::Response& res) {
+        change_pw.handle_Change_PW(req, res);
         });
 
     // CORS 설정
